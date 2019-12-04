880499a05f126af1003fc8d0da5888650d5a2493
ShewinVN <levantrungeale28595@gmail.com>
2019-12-04 19:02:06 +0700

    Add method login

--- lib/LoginPage.dart
+++ lib/LoginPage.dart
@@ -3,20 +3,15 @@ import 'package:flutter/services.dart';
 import 'stacked_icons.dart';
 import 'home.dart';
 
-
-
-
 class LoginPage extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
-    
-SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
-  statusBarColor: Colors.orange, //or set color with: Color(0xFF0000FF)
-));
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
+      statusBarColor: Colors.orange, //or set color with: Color(0xFF0000FF)
+    ));
     return new Scaffold(
       appBar: new AppBar(
-        backgroundColor:Colors.transparent,
+          backgroundColor: Colors.transparent,
           elevation: 0.0,
           iconTheme: new IconThemeData(color: Color(0xFF18D191))),
       body: Container(
@@ -57,19 +52,22 @@ SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
                         left: 20.0, right: 5.0, top: 10.0),
                     child: GestureDetector(
                       onTap: () {
-                        Navigator.push(context, MaterialPageRoute(
-                          builder: (context) => HomePage()
-                        ));
+                        if (checkLogin("admin","123")) {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => HomePage()));
+                        }
                       },
-                                          child: new Container(
-                            alignment: Alignment.center,
-                            height: 60.0,
-                            decoration: new BoxDecoration(
-                                color: Color(0xFF18D191),
-                                borderRadius: new BorderRadius.circular(9.0)),
-                            child: new Text("Login",
-                                style: new TextStyle(
-                                    fontSize: 20.0, color: Colors.white))),
+                      child: new Container(
+                          alignment: Alignment.center,
+                          height: 60.0,
+                          decoration: new BoxDecoration(
+                              color: Color(0xFF18D191),
+                              borderRadius: new BorderRadius.circular(9.0)),
+                          child: new Text("Login",
+                              style: new TextStyle(
+                                  fontSize: 20.0, color: Colors.white))),
                     ),
                   ),
                 ),
@@ -92,9 +90,12 @@ SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
                   Padding(
-                    padding: const EdgeInsets.only(bottom:18.0),
-                    child: new Text("Create A New Account ",style: new TextStyle(
-                                fontSize: 17.0, color: Color(0xFF18D191),fontWeight: FontWeight.bold)),
+                    padding: const EdgeInsets.only(bottom: 18.0),
+                    child: new Text("Create A New Account ",
+                        style: new TextStyle(
+                            fontSize: 17.0,
+                            color: Color(0xFF18D191),
+                            fontWeight: FontWeight.bold)),
                   ),
                 ],
               ),
@@ -103,5 +104,11 @@ SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
         ),
       ),
     );
+  }
+
+  bool checkLogin(String user, String pass) {
+    bool boo = false;
+    if (user == "admin" && pass == "123") boo = true;
+    return boo;
   }
 }
